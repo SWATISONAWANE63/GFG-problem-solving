4fd27037d1e5b65c410a44fa0685964af184368e
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-05 17:04:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,18 @@
+// function Template for C++
+
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        // code here.
+        stack<int>s;
+        while(!q.empty()){
+            s.push(q.front());
+            q.pop();
+        }
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+    }
+};
